5846a5218d747822f76117dab780cc2d81bdd78a
Jomar Anthony Sastrillo <sastrill@alumni.ubc.ca>
2017-12-03 21:57:50 -0800

    Added script to download data

---
+++ src/get-data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+curl -o data/education.xls https://www.ers.usda.gov/webdocs/DataFiles/48747/Education.xls?v=42762
+
+curl -o data/poverty.xls https://www.ers.usda.gov/webdocs/DataFiles/48747/PovertyEstimates.xls?v=42762
